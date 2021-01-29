d27ddfc02079a7816be3882e0100d7ff223a9882
Vincent <vincent@BULLSEYE-debian-live-builder>
2021-01-29 22:47:52 +0100

    add vpn

--- vmware/vmware_modules.sh
+++ vmware/vmware_modules.sh
@@ -16,7 +16,6 @@ make
 sudo make install
 
 sudo rm /usr/lib/vmware/lib/libz.so.1/libz.so.1
-sudo ln -s /lib/x86_64-linux-gnu/libz.so.1
-/usr/lib/vmware/lib/libz.so.1/libz.so.1
+sudo ln -s /lib/x86_64-linux-gnu/libz.so.1 /usr/lib/vmware/lib/libz.so.1/libz.so.1
 
 sudo /etc/init.d/vmware restart
